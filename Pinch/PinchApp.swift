cf8c86f0064986f70859a6d569e2059506b695b4
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-05 20:59:08 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 飯塚政美 on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
